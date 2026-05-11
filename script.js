0b1dea5427aba54e08f2f550e056b554ee8791bb
Zulimans30 <138149760+Zulimans30@users.noreply.github.com>
2026-05-11 09:41:18 -0400

    Update script.js
    Cambio de descripcion

--- script.js
+++ script.js
@@ -92,8 +92,8 @@ function showDedicationText() { //seguidores
 // Firma manuscrita animada
 function showSignature() {
   // Cambia para buscar la firma dentro del contenedor de dedicatoria
-  const dedication = document.getElementById('dedication-text');
-  let signature = dedication.querySelector('#signature');
+  const dedication = document.getElementById('Te amo doce Millones a la decima potencia mi Clarett');
+  let signature = dedication.querySelector('De Zulimans');
   if (!signature) {
     signature = document.createElement('div');
     signature.id = 'signature';
